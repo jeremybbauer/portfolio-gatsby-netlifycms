7e43fe6108da8db0fdc1be0474fcc86c5ea4692f
Jeremy Bauer <jeremybbauer@gmail.com>
2020-10-29 23:52:49 +0000

    Update footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,11 +1,13 @@
 import React from "react"
-import {Link} from "gatsby"
+import { Link } from "gatsby"
 import { RiHeart2Line } from "react-icons/ri";
 
 const Footer = () => (
   <footer className="site-footer">
     <div className="container">
-      <p>&copy; <script>new Date().getFullYear()>2010&&document.write(" "+new Date().getFullYear())</script>, Jeremy Bauer. Thanks for visiting <span className="icon -love"><RiHeart2Line/></span></p>
+      <p>&copy; <script type="text/javascript">
+        document.write(new Date().getFullYear());
+</script>, Jeremy Bauer. Thanks for visiting <span className="icon -love"><RiHeart2Line /></span></p>
     </div>
   </footer>
 )
